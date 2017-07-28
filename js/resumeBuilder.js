3f154647e41ecc80295b98105a0de087105798b0
thelastfantasy <kawasumi.pks@outlook.com>
2017-07-28 14:50:51 +0800

    detail

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,6 +2,41 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 //$('#main').append('pikashi');
-var formattedName = HTMLheaderName.replace('%data%','Pikashi');
-var formattedRole = HTMLheaderRole.replace('%data%','Front-end Engineer');
-$('#header').prepend(formattedRole).prepend(formattedName);
+// var formattedName = HTMLheaderName.replace('%data%','Pikashi');
+// var formattedRole = HTMLheaderRole.replace('%data%','Front-end Engineer');
+var bio = {
+    'name': 'Tao Yulong',
+    'role': 'Front-end Engineer',
+    'contacts':{
+        'mobile':'18616310150',
+        'email':'taoyulong@gmail.com',
+        'github':'thelastfantasy',
+        'twitter':'@pikashi',
+        'location':'Shanghai'
+    },
+    'pic': './images/me.jpg',
+    'welcomeMessage': 'Welcome! Here is my bio page.',
+    'skills': ['CSS','Javascript','jQuery']
+};
+var formattedName = HTMLheaderName.replace('%data%',bio.name);
+var formattedRole = HTMLheaderRole.replace('%data%',bio.role);
+var formattedMobile = HTMLmobile.replace('%data%',bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace('%data%',bio.contacts.email);
+var formattedGithub = HTMLgithub.replace('%data%',bio.contacts.github);
+var formattedTwitter = HTMLtwitter.replace('%data%',bio.contacts.twitter);
+var formattedLocation = HTMLlocation.replace('%data%',bio.contacts.location);
+var formattedPic = HTMLbioPic.replace('%data%',bio.pic);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage);
+var formattedSkills = HTMLskills.replace('%data%',bio.skills.join(', '));
+
+$('#header')
+    .prepend(formattedSkills)
+    .prepend(formattedPic)
+    .prepend(formattedLocation)
+    .prepend(formattedTwitter)
+    .prepend(formattedGithub)
+    .prepend(formattedEmail)
+    .prepend(formattedMobile)
+    .prepend(formattedWelcomeMsg)
+    .prepend(formattedRole)
+    .prepend(formattedName);
