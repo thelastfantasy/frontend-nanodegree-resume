557e26ccb8533d9fcc912587f40c2dc929011b89
thelastfantasy <kawasumi.pks@outlook.com>
2017-07-28 11:48:41 +0800

    fixx

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,4 +4,4 @@ This is empty on purpose! Your code to build the resume will go here.
 //$('#main').append('pikashi');
 var formattedName = HTMLheaderName.replace('%data%','Pikashi');
 var formattedRole = HTMLheaderRole.replace('%data%','Front-end Engineer');
-$('#header').append(formattedName).append(formattedRole);
+$('#header').prepend(formattedRole).prepend(formattedName);
