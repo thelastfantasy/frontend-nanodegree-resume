2ca766ab2e28b05a7baf7964dbf89376588d205a
thelastfantasy <kawasumi.pks@outlook.com>
2017-07-31 14:47:43 +0800

    complete

--- js/helper.js
+++ js/helper.js
@@ -62,10 +62,10 @@ var googleMap = '<div id="map"></div>';
 /*
 The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
-$(document).ready(function() {
-  $('button').click(function() {
+$(document).ready(function () {
+  $('button').click(function () {
     var $name = $('#name');
-    var iName = inName($name.text()) || function(){};
+    var iName = inName($name.text()) || function () {};
     $name.html(iName);
   });
 });
@@ -75,17 +75,15 @@ The next few lines about clicks are for the Collecting Click Locations quiz in t
 */
 var clickLocations = [];
 
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
+function logClicks(x, y) {
+  clickLocations.push({
+    x: x,
+    y: y
+  });
   console.log('x location: ' + x + '; y location: ' + y);
 }
 
-$(document).click(function(loc) {
+$(document).click(function (loc) {
   // your code goes here!
 });
 
@@ -96,7 +94,7 @@ This is the fun part. Here's where we generate the custom Google Map for the web
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
-var map;    // declares a global map variable
+var map; // declares a global map variable
 
 
 /*
@@ -133,7 +131,7 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
+    education.schools.forEach(function (school) {
       locations.push(school.location);
     });
 
@@ -141,7 +139,7 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    work.jobs.forEach(function (job) {
       locations.push(job.location);
     });
 
@@ -156,10 +154,10 @@ function initializeMap() {
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+    var lat = placeData.geometry.location.lat(); // latitude from the place service
+    var lon = placeData.geometry.location.lng(); // longitude from the place service
+    var name = placeData.formatted_address; // name of the place from the place service
+    var bounds = window.mapBounds; // current boundaries of the map window
 
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
@@ -176,8 +174,10 @@ function initializeMap() {
     });
 
     // hmmmm, I wonder what this is about...
-    google.maps.event.addListener(marker, 'click', function() {
+    google.maps.event.addListener(marker, 'click', function () {
       // your code goes here!
+      infoWindow.setContent('这里是' + name);
+      infoWindow.open(map, this);
     });
 
     // this is where the pin actually gets added to the map.
@@ -210,7 +210,7 @@ function initializeMap() {
     var service = new google.maps.places.PlacesService(map);
 
     // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
+    locations.forEach(function (place) {
       // the search request object
       var request = {
         query: place
@@ -239,11 +239,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function (e) {
+  // Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,7 +15,25 @@ var work = {
         'dates': '2015 - April 2017',
         'location': 'Yangzhou',
         'description': 'Work in this B2C company, the salary is not bad but the work is boring and repetity, I don\'t like it.'
-    }]
+    }],
+    display: function () {
+        for (var job in work.jobs) {
+            $('#workExperience').append(HTMLworkStart);
+
+            var formattedWorkEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+            var formattedWorkTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
+            var formatedEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
+            $('.work-entry:last').append(formatedEmployerTitle);
+
+            var formattedWorkDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
+            $('.work-entry:last').append(formattedWorkDates);
+            var formattedWorkLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
+            $('.work-entry:last').append(formattedWorkLocation);
+
+            var formattedWorkDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
+            $('.work-entry:last').append(formattedWorkDescription);
+        }
+    }
 }
 
 var projects = {
@@ -23,8 +41,8 @@ var projects = {
             "title": "RS company",
             "dates": "2016",
             "description": "Website design for RS company",
-            "images": ["images/197x148.gif","images/197x148.gif"],
-            "link": "http://www.oushi.com.cn/"
+            "images": ["images/197x148.gif", "images/197x148.gif"],
+            "link": "http://china.rs-online.com/web/"
         },
         {
             "title": "Xiongshida Company",
@@ -33,7 +51,23 @@ var projects = {
             "images": ["images/197x148.gif"],
             "link": "http://xiongshida.cn/"
         }
-    ]
+    ],
+    display: function () {
+        for (var i = 0; i < projects.projects.length; i++) {
+            $('#projects').append(HTMLprojectStart);
+            var projTitle = HTMLprojectTitle.replace("%data%", "<a href=\"" + projects.projects[i].link + "\" target=\"_bkank\">" + projects.projects[i].title + "</a>");
+            $('.project-entry:last').append(projTitle);
+            var projDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+            $('.project-entry:last').append(projDates);
+            var projDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+            $('.project-entry:last').append(projDescription);
+            if (projects.projects[i].images.length > 0) {
+                for (var j = 0; j < projects.projects[i].images.length; j++) {
+                    $('.project-entry:last').append(HTMLprojectImage.replace("%data%", projects.projects[i].images[j]));
+                }
+            }
+        }
+    }
 };
 
 var bio = {
@@ -48,7 +82,37 @@ var bio = {
         'twitter': '@pikashi',
         'location': 'Shanghai'
     },
-    'skills': ['CSS', 'Javascript', 'jQuery']
+    'skills': ['CSS', 'Javascript', 'jQuery'],
+    display: function () {
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+        var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+        var formattedContactInfo = [];
+        formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+        formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+        formattedContactInfo.push(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+        formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+
+        $("#header").prepend(formattedRole);
+        $("#header").prepend(formattedName);
+        $("#header").append(formattedBioPic);
+        $("#header").append(formattedWelcomeMsg);
+
+        if (bio.skills.length > 0) {
+            $("#header").append(HTMLskillsStart);
+
+            for (i in bio.skills) {
+                $("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+            }
+        }
+
+        for (i in formattedContactInfo) {
+            $("#topContacts").append(formattedContactInfo[i]);
+            $("#footerContacts").append(formattedContactInfo[i]);
+        }
+    }
 };
 
 var education = {
@@ -56,71 +120,59 @@ var education = {
         'name': 'Jinken College of Technology',
         'dates': '3 years',
         'location': 'Nanjing',
-        'degree': '',
-        'url': '',
-        'majors': [],
+        'degree': '2007 - 2010',
+        'url': 'http://www.jku.edu.cn/',
+        'majors': ['Electronic Communications'],
     }],
     'onlineCourses': [{
-        'title': '',
-        'school': '',
-        'dates': '',
-        'url': ''
-    }]
+        'title': 'Udacity Front-end Nanodegree Basic',
+        'school': 'Udacity',
+        'dates': '2017',
+        'url': 'http://www.udacity.com'
+    }],
+    display: function () {
+        if (education.schools.length > 0 || education.onlineCourses.length > 0) {
+            for (i in education.schools) {
+                $("#education").append(HTMLschoolStart);
+
+                var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+                var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+                var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+                var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+                var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors.join(', '));
+
+                $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+                $(".education-entry:last").append(formattedSchoolDates);
+                $(".education-entry:last").append(formattedSchoolLocation);
+                $(".education-entry:last").append(formattedSchoolMajor);
+            }
+
+            if (education.onlineCourses.length > 0) {
+                $("#education").append(HTMLonlineClasses);
+                for (i in education.onlineCourses) {
+                    $("#education").append(HTMLschoolStart);
+                    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
+                    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+                    var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
+                    var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
+
+                    $(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+                    $(".education-entry:last").append(formattedOnlineDates);
+                    $(".education-entry:last").append(formattedOnlineURL);
+                }
+            }
+
+        }
+    }
 };
 
+work.display();
+projects.display();
+bio.display();
+education.display();
+$("#mapDiv").append(googleMap);
 
-if (bio.skills.length !== 0) {
-    $('#header').append(HTMLskillsStart);
-
-    var formattedSkills = HTMLskills.replace('%data%', bio.skills[0]);
-    $('#skills').append(formattedSkills);
-    var formattedSkills = HTMLskills.replace('%data%', bio.skills[1]);
-    $('#skills').append(formattedSkills);
-    var formattedSkills = HTMLskills.replace('%data%', bio.skills[2]);
-    $('#skills').append(formattedSkills);
-}
-
-
-
-var formattedName = HTMLheaderName.replace('%data%', bio.name);
-var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
-var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
-var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
-var formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
-var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
-var formattedPic = HTMLbioPic.replace('%data%', bio.biopic);
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
-var allBioInfo = formattedName + formattedRole;
-var allContacts = formattedMobile + formattedEmail + formattedGithub + formattedTwitter + formattedLocation;
-var moreinfo = formattedPic + formattedWelcomeMsg;
-
-$('#topContacts').append(allContacts);
-$('#topContacts').append(moreinfo);
-$('#topContacts').prepend(allBioInfo);
-
-function displayWork() {
-    for (var job in work.jobs) {
-        $('#workExperience').append(HTMLworkStart);
-
-        var formattedWorkEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
-        var formattedWorkTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
-        var formatedEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
-        $('.work-entry:last').append(formatedEmployerTitle);
-
-        var formattedWorkDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
-        $('.work-entry:last').append(formattedWorkDates);
-        var formattedWorkLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
-        $('.work-entry:last').append(formattedWorkLocation);
-
-        var formattedWorkDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
-        //var allWorkInfo = formattedWorkEmployer + formattedWorkTitle + formattedWorkDates + formattedWorkLocation + formattedWorkDescription;
-        $('.work-entry:last').append(formattedWorkDescription);
-    }
-}
-displayWork();
-
-$(document).click(function (e) {
+/* $(document).click(function (e) {
     var x = e.pageX;
     var y = e.pageY;
 
@@ -134,23 +186,4 @@ function inName(name) {
     var lastName = arr[1].toUpperCase();
     return firstName + ' ' + lastName;
 }
-$('#main').append(internationalizeButton);
-
-projects.display = function () {
-    for (var i = 0; i < projects.projects.length; i++) {
-        $('#projects').append(HTMLprojectStart);
-        var projTitle = HTMLprojectTitle.replace("%data%", "<a href=\"" + projects.projects[i].link + "\" target=\"_bkank\">" + projects.projects[i].title + "</a>");
-        $('.project-entry:last').append(projTitle);
-        var projDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
-        $('.project-entry:last').append(projDates);
-        var projDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
-        $('.project-entry:last').append(projDescription);
-        if (projects.projects[i].images.length > 0) {
-            for (var j = 0; j < projects.projects[i].images.length; j++) {
-                $('.project-entry:last').append(HTMLprojectImage.replace("%data%", projects.projects[i].images[j]));
-            }
-        }
-    }
-};
-
-projects.display();
+$('#main').append(internationalizeButton); */
